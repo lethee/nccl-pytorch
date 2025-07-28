bfcabbb792da0d22ef780416fb006d166fe4c029
sh.yee <sh.yee@42dot.ai>
2025-07-28 21:06:02 +0900

    update script

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 export OMP_NUM_THREADS=1
-
+id
+ulimit -a
+nvidia-smi -L
+ibv_devinfo -l
+ls -al /dev/infiniband
+printenv | sort
 torchrun \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
@@ -7,3 +12,4 @@ torchrun \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
     /host/nccl_perf_test.py
+sleep 10
